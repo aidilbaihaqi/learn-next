71cf508a395d39876dd2b704bcef035fd7f461ef
aidilbaihaqi <aidillmusirjun@gmail.com>
2025-01-08 01:02:32 +0700

    learn: folder structure, react components, import components, simple routing

---
+++ first-app/app/components/Greeting.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Greeting() {
+  return (
+    <h1 className="text-3xl font-bold text-center">This is my first next.js app</h1>
+  )
+}
+
+export default Greeting
---
+++ first-app/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Greeting from '@/app/components/Greeting'
+
+const Page = () => {
+  return (
+    <>
+      <div>
+        <Greeting />
+        <p className='text-center my-2'>This is contact page</p>
+      </div>
+    </>
+  )
+}
+
+export default Page
